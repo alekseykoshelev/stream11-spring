5de316a9a58ea7b9f39175965544807d322859bd
Aleksey Koshelev <koshelev.alexej@gmail.com>
2023-09-09 13:22:14 +0400

    Compile fix

--- src/main/java/com/example/ind11collections/Ind11CollectionsApplication.java
+++ src/main/java/com/example/ind11collections/Ind11CollectionsApplication.java
@@ -1,22 +1,13 @@
 package com.example.ind11collections;
 
-import com.example.ind11collections.model.Employee;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Ind11CollectionsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Ind11CollectionsApplication.class, args);
-        // javafx
-        // swing
-
-        ArrayList<String> a;
-        a.equals()
     }
 
 }
